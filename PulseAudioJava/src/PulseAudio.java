2102c4ca267a0da6cc3d090c8a3e76121e183ed0
jkody <ghtukjk@gmail.com>
2018-10-09 16:41:20 -0500

    Created example

--- PulseAudioJava/src/PulseAudio.java
+++ PulseAudioJava/src/PulseAudio.java
@@ -7,13 +7,6 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.concurrent.Semaphore;
 
-/**
- * @title "PulseAudio"
- * 
- * @version "0.1.1"
- * @since September 2018 
- *
- */
 public class PulseAudio {
 	public static final String module = "PulseAudio";
 	public static final int ABORT_COUNTER_MAX = 500;
